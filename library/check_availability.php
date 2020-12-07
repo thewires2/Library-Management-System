0a678d015b26943479ddc6b031b393791ff4be62
Arnav Bagchi <2018.arnav.bagchi@ves.ac.in>
2020-12-07 21:02:56 +0530

    Update check_availability.php

--- library/check_availability.php
+++ library/check_availability.php
@@ -1,6 +1,5 @@
 <?php 
 require_once("includes/config.php");
-// code user email availablity
 if(!empty($_POST["emailid"])) {
 	$email= $_POST["emailid"];
 	if (filter_var($email, FILTER_VALIDATE_EMAIL)===false) {
