99f1f21f9a443b148a93e2250657f657fb00ec10
Jay Dulera <53439144+jaydulera@users.noreply.github.com>
2020-12-10 18:13:56 +0530

    Update config.php

--- library/includes/config.php
+++ library/includes/config.php
@@ -7,6 +7,7 @@ define('DB_NAME','library');
 
 try
 {
+$conn = new PDO("mysql:host=DB_NAME;port=8889;dbname=AppDatabase", DB_USER, DB_PASS); 
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
 catch (PDOException $e)
